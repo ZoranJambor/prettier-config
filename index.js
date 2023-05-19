6c2ad8bbf5c700a0dd66a0e2e0765972e3af8e58
Zoran Jambor <info@zoranjambor.com>
2023-05-19 14:13:23 +0200

    Add initial Prettier config

---
+++ index.js
@@ -0,0 +1,37 @@
+/** @format */
+
+// prettier.config.js or .prettierrc.js
+module.exports = {
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'off',
+	htmlWhitespaceSensitivity: 'ignore',
+	insertPragma: true,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	requirePragma: true,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 20,
+	trailingComma: 'es5',
+	useTabs: true,
+	vueIndentScriptAndStyle: false,
+	singleAttributePerLine: true,
+	overrides: [
+		{
+			files: '*.html',
+			options: {
+				singleAttributePerLine: false,
+			},
+		},
+		{
+			files: 'C02',
+			options: {
+				parser: "html",
+			},
+		},
+	],
+}
